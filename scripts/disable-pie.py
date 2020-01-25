d69b8f4db36f1ada37bc89c36f7499801aa9dd2b
phra <greensoncio@gmail.com>
2020-01-25 02:34:42 +0100

    update disable pie script

--- scripts/disable-pie.py
+++ scripts/disable-pie.py
@@ -4,15 +4,20 @@ import sys
 import lief
 
 if len(sys.argv) < 2:
-    print('Usage: python3 {} <FILE>'.format(sys.argv[0]))
-    sys.exit(-1)
+  print('Usage: python3 {} <FILE>'.format(sys.argv[0]))
+  sys.exit(-1)
 
 filename = sys.argv[1]
 
 app = lief.parse(filename)
 
 if app.is_pie:
-    print('Binary is PIE, disabling it')
+  print('Binary is PIE, disabling it')
+  if isinstance(app, lief.MachO.Binary):
     app.header.remove(lief.MachO.HEADER_FLAGS.PIE)
+  else:
+    raise Exception('Format not supported')
+else:
+  print('Binary is not PIE')
 
 app.write(filename)
